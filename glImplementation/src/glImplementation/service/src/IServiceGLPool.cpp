fa6b658293317a0298c5b03efe28be89cf56eac2
sushilmiitb <sushil@chymeravr.com>
2016-12-01 20:28:58 +0530

    Removing everything

--- glImplementation/src/glImplementation/service/src/IServiceGLPool.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by chimeralabs on 10/28/2016.
-//
-
-#include <glImplementation/service/IServiceGLPool.h>
-
-namespace cl{
-    std::unique_ptr<IServiceGLPool> IServiceGLPool::serviceGLPoolPtr = nullptr;
-}
