1b088cb7adcea44f5118171739d40fc310d1798e
sushilmiitb <sushil@chymeravr.com>
2016-12-04 19:18:27 +0530

    Added testApp project structure

---
+++ TestApp/include/testApp/TestApp.h
@@ -0,0 +1,30 @@
+#ifndef TESTAPP_TESTAPP_H
+#define TESTAPP_TESTAPP_H
+
+#include <coreEngine/IRenderer.h>
+#include <coreEngine/events/AppEventListener.h>
+
+#include <coreEngine/factory/IModelFactory.h>
+#include <coreEngine/factory/ITextureFactory.h>
+#include <coreEngine/factory/IMaterialDiffuseTextureFactory.h>
+#include <coreEngine/factory/IShaderDiffuseTextureFactory.h>
+
+#include <coreEngine/components/transform/ITransformCameraFactory.h>
+#include <coreEngine/components/transform/ITransformModelFactory.h>
+
+namespace cl{
+    class TestApp : public AppEventListener{
+    public:
+        TestApp(std::unique_ptr<IRenderer> renderer, std::unique_ptr<IModelFactory> modelFactory, std::unique_ptr<ITextureFactory> textureFactory,
+            std::unique_ptr<IMaterialDiffuseTextureFactory> materialDiffuseTextureFactory, std::unique_ptr<IShaderDiffuseTextureFactory> shaderDiffuseTextureFactory,
+            std::unique_ptr<ITransformCameraFactory> transformCameraFactory, std::unique_ptr<ITransformModelFactory> transformModelFactory);
+
+        //AppEventListener implementation
+        void onAppStartEvent();
+        void onAppPauseEvent();
+        void onAppResumeEvent();
+        void onAppStopEvent();
+    };
+}
+
+#endif //TESTAPP_TESTAPP_H
