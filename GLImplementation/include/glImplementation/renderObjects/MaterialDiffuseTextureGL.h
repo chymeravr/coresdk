11ff3044d2894342103da6c7db0e28b010158fe2
sushilmiitb <sushil@chymeravr.com>
2017-01-25 16:44:28 +0530

    Readded some files

---
+++ GLImplementation/include/glImplementation/renderObjects/MaterialDiffuseTextureGL.h
@@ -0,0 +1,24 @@
+#ifndef GLIMPLEMENTATION_MATERIALDIFFUSETEXTUREGL_H
+#define GLIMPLEMENTATION_MATERIALDIFFUSETEXTUREGL_H
+
+#include <coreEngine/renderObjects/MaterialDiffuseTexture.h>
+#include <coreEngine/renderObjects/IRenderable.h>
+#include <conf/GLTypes.h>
+#include <glImplementation/renderObjects/ShaderDiffuseTextureGL.h>
+
+namespace cl{
+    class MaterialDiffuseTextureGL : public MaterialDiffuseTexture, public IRenderable{
+    public:
+        MaterialDiffuseTextureGL(const std::string &sceneId, ShaderDiffuseTextureGL *shader, ILoggerFactory *loggerFactory);
+        IRenderable *getRenderable();
+        void setDiffuseTextureId(CL_GLuint diffuseTextureUniformId);//To be set by respective shader
+        bool initialize();
+        void draw();
+        void deinitialize();
+    private:
+        std::unique_ptr<ILogger> logger;
+        CL_GLuint diffuseTextureUniformId;
+    };
+}
+
+#endif //GLIMPLEMENTATION_MATERIALDIFFUSETEXTUREGL_H
