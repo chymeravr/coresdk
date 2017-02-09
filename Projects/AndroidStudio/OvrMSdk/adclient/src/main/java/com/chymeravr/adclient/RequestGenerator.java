2d0d90e9165a228a9828d9311d8163e64402a4da
robin <robin@chymeravr.com>
2017-02-09 09:46:05 +0530

    removed hard coded lib dependency from schema - its packaged as a jar file in adclient libs

--- Projects/AndroidStudio/OvrMSdk/adclient/src/main/java/com/chymeravr/adclient/RequestGenerator.java
+++ Projects/AndroidStudio/OvrMSdk/adclient/src/main/java/com/chymeravr/adclient/RequestGenerator.java
@@ -62,77 +62,6 @@ final class RequestGenerator {
 
         String appId = ChymeraVrSdk.getApplicationId();
 
-//        List<Placement> placementList = new ArrayList<>();
-//        placementList.add(new Placement(this.getPlacementId(), this.getAdType()));
-//
-//        int hmdId = Config.hmdId;
-//
-//        Demographic userDemo = new Demographic(adRequest.getBirthday().toString(),
-//                adRequest.getGender().getValue(), "sample@sample.com");
-//
-//        UserLocation userLoc = new UserLocation(adRequest.getLocation().getLatitude(),
-//                adRequest.getLocation().getLongitude(), adRequest.getLocation().getAccuracy());
-//
-//        ActivityManager actManager = (ActivityManager) ChymeraVrSdk.getContext().getSystemService(ACTIVITY_SERVICE);
-//        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
-//        actManager.getMemoryInfo(memInfo);
-//        long totalMemory = memInfo.totalMem;
-//
-//        DeviceInfo deviceInfo = new DeviceInfo(Build.MANUFACTURER, Build.MODEL, Double.toString(totalMemory));
-//
-//        String connectivity = null;
-//        ConnectivityManager cm = (ConnectivityManager) ChymeraVrSdk.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-//        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-//        if (activeNetwork != null) { // connected to the internet
-//            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
-//                // connected to wifi
-//                connectivity = "Wifi";
-//            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
-//                // connected to the mobile provider's data plan
-//                connectivity = "Mobile";
-//            }
-//        } else {
-//            // not connected to the internet
-//            Log.e("RequestGenerator", "User currently not connected to the internet");
-//        }
-//
-//        WifiManager wifiManager = (WifiManager) ChymeraVrSdk.getContext().getSystemService(Context.WIFI_SERVICE);
-//        WifiInfo info = wifiManager.getConnectionInfo();
-//        String ssid = info.getSSID();
-//
-//        Gson gson = new Gson();
-////        JsonArray placementListJson = new JsonArray(); //gson.toJson(placementList);
-//        String placementListArray[] = new String[1];
-//        for (Placement placement : placementList) {
-////            HashMap<String, String> placementMap = new HashMap<>();
-////            placementMap.put("adFormat", this.getAdType().toString());
-////            placementMap.put("id", this.getPlacementId());
-//            placementListArray[0] = gson.toJson(placement);
-//        }
-//
-//        String userDemoJson = gson.toJson(userDemo);
-//        String userLocJson = gson.toJson(userLoc);
-//        String deviceInfoJson = gson.toJson(deviceInfo);
-//
-//        JSONObject jsonRequest = new JSONObject();
-//        try {
-//            jsonRequest.put("sdkVersion", sdkVersion);
-//            jsonRequest.put("timestamp", timestamp);
-//            jsonRequest.put("appId", ChymeraVrSdk.getApplicationId());
-//            jsonRequest.put("placements", placementListJson);
-//            jsonRequest.put("osId", Config.osId);
-//            jsonRequest.put("osVersion", Build.VERSION.SDK_INT);
-//            jsonRequest.put("userId", ChymeraVrSdk.getAdvertisingId());
-//            jsonRequest.put("hmdId", hmdId);
-//            jsonRequest.put("location", userLocJson);
-//            jsonRequest.put("demographics", userDemoJson);
-//            jsonRequest.put("deviceInfo", deviceInfoJson);
-//            jsonRequest.put("connectivity", connectivity);
-//            jsonRequest.put("wifiName", ssid);
-//        } catch (JSONException e) {
-//            Log.e("RequestGenerator", "Unable to form jsonRequest : ", e);
-//        }
-
         List<Placement> placements = new ArrayList<Placement>();
         placements.add(new Placement(this.getPlacementId(), this.getAdType()));
 
@@ -175,17 +104,15 @@ final class RequestGenerator {
                         ChymeraVrSdk.getAdvertisingId(), Config.hmdId,
                         loc, demo, device, connectivity, ssid);
 
-        Gson gson = new Gson();
-
         String servingRequestJson = new Gson().toJson(servingRequest);
-        JSONObject test = null;
+        JSONObject jsonAdRequest = null;
         try {
-            test = new JSONObject(servingRequestJson);
+            jsonAdRequest = new JSONObject(servingRequestJson);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         JsonObjectRequest requestResult = new JsonObjectRequest(Request.Method.POST, Config.adServer,
-                test, jsonServerListener, jsonServerListener) {
+                jsonAdRequest, jsonServerListener, jsonServerListener) {
             // we need to overwrite the default content type.
             @Override
             public String getBodyContentType() {
